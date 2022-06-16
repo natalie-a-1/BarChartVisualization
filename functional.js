8478657a43d702c301732c861f2c8886f1960686
nataliehill <natalie.a.hill-1@ou.edu>
2022-06-15 20:27:05 -0500

    cleaned code

--- functional.js
+++ functional.js
@@ -1,19 +1,19 @@
 
-    //grabbing data from API and storing in values
-    var height = 830;
-    var width = 1504;
-    var padding = 70;
+//grabbing data from API and storing in values
+var height = 830;
+var width = 1504;
+var padding = 70;
 
-    var hScale;
-    var wScale;
-    var yScale
-    var xScale;
-    var svg;
-    var dates;
+var hScale;
+var wScale;
+var yScale
+var xScale;
+var svg;
+var dates;
 
 
-    var values = [];
-    var data = d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+var values = [];
+var data = d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(data => {
         values = data.data;
         svgBox();
@@ -22,76 +22,76 @@
         createRect();
     });
 
-    //creating svg
-     var svgBox = () => {
-        d3.select('#barArea')
+//creating svg
+var svgBox = () => {
+    d3.select('#barArea')
         .append('svg')
-        .attr('width', (width/2)+padding)
+        .attr('width', (width / 2) + padding)
         .attr('height', height)
         .attr('overflow', 'hidden');
 
-        svg = d3.select('svg');
-        window.scrollTo(0,0);
+    svg = d3.select('svg');
+    window.scrollTo(0, 0);
 
-    };
+};
 
-    //create scales
-    var scales = () => {
-        hScale = d3.scaleLinear()
+//create scales
+var scales = () => {
+    hScale = d3.scaleLinear()
         .domain([0, d3.max(values, (item) => {
             return item[1];
         })])
-        .range([0, height-(2*padding)]);
+        .range([0, height - (2 * padding)]);
 
-        wScale = d3.scaleLinear()
-        .domain([0, values.length -1])
-        .range([padding, (width/2)]);
+    wScale = d3.scaleLinear()
+        .domain([0, values.length - 1])
+        .range([padding, (width / 2)]);
 
-        dates = values.map((item) => {
-            return new Date(item[0]);
-        });
+    dates = values.map((item) => {
+        return new Date(item[0]);
+    });
 
-        xScale = d3.scaleTime()
+    xScale = d3.scaleTime()
         .domain(d3.extent(dates))
-        .range([padding, (width/2)]);
+        .range([padding, (width / 2)]);
 
-        yScale = d3.scaleLinear()
+    yScale = d3.scaleLinear()
         .domain([0, d3.max(values, (item) => {
             return item[1];
         })])
         .range([height - padding, padding]);
-    };
+};
 
-    //create axes
-    var axes = () => {
-        let xAxis = d3.axisBottom(xScale);
+//create axes
+var axes = () => {
+    let xAxis = d3.axisBottom(xScale);
 
-        svg.append('g')
+    svg.append('g')
         .call(xAxis)
         .attr('id', 'x-axis')
         .attr('transform', 'translate(0,' + (height - padding) + ')');
 
-        let yAxis = d3.axisLeft(yScale);
+    let yAxis = d3.axisLeft(yScale);
 
-        svg.append('g')
+    svg.append('g')
         .call(yAxis)
         .attr('id', 'y-axis')
         .attr('transform', 'translate(' + padding + ',0)');
-    };
+};
 
-    //create bars
-    var createRect = () => {
+//create bars
+var createRect = () => {
 
-        let tooltip = d3.selectAll('#tooltip')
+    let tooltip = d3.selectAll('#tooltip')
         .style('visibility', 'hidden')
         .attr('class', 'item');
-        
-        svg.selectAll('rect')
+
+    svg.selectAll('rect')
         .data(values)
         .enter()
         .append('rect')
         .attr('class', 'bar')
-        .attr('width', ((width/2) - (2 * padding)) / values.length)
+        .attr('width', ((width / 2) - (2 * padding)) / values.length)
         .attr('data-date', (item) => {
             return item[0];
         })
@@ -104,27 +104,27 @@
         .attr('x', (item, index) => {
             return wScale(index);
         })
-        .attr('y', (item) => { 
+        .attr('y', (item) => {
             return (height - padding) - hScale(item[1]);
         })
         .on('mouseover', function (item) {
-                tooltip.transition()
-                    .style('visibility', 'visible')
-                    .style('position', 'relative');
+            tooltip.transition()
+                .style('visibility', 'visible')
+                .style('position', 'relative');
 
-                document.querySelector('#tooltip').setAttribute('data-date', item[0]);
+            document.querySelector('#tooltip').setAttribute('data-date', item[0]);
 
-                tooltip.html("$ " + item[1] + " billion" + '<br>' + "" + item[0])
+            tooltip.html("$ " + item[1] + " billion" + '<br>' + "" + item[0])
                 .style('font-weight', 1000 + 'px')
 
-                tooltip.style('left', 220 + 'px')
+            tooltip.style('left', 220 + 'px')
                 .style('top', 400 + 'px')
                 .style('transform', 'translateX(60px)');
-                
-            })
+
+        })
         .on('mouseout', function (item) {
             tooltip.transition()
                 .style('visibility', 'hidden');
         })
-        window.scrollTo(0,0);
-    };
+    window.scrollTo(0, 0);
+};
